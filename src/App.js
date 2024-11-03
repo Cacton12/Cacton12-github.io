ce5e759b32e24c2daa558502a2fe65e114fc24a3
Cacton12 <Colbyacton12@icloud.com>
2024-11-03 14:54:34 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import NavBar from "./Navbar";
+import Main from "./Main";
+import Footer from "./Footer";
+import "./index.css";
+
+function App() {
+  return (
+      /* Background */
+      <div
+        className="relative w-screen h-screen bg-cover bg-center flex flex-col"
+        style={{ backgroundImage: "url('/background.jpg')" }}
+      >
+      {/* Background Overlay */}
+      <div className="absolute inset-0 bg-gradient-to-b from-black/50 to-black"></div>
+
+      {/* Navbar */}
+      <div className="relative z-10">
+        <NavBar></NavBar>
+      </div>
+
+      {/* Main Content */}
+      <div className="relative z-10 flex-grow">
+        <Main></Main>
+      </div>
+
+      {/* Footer */}
+      <div className="relative z-10 mt-auto">
+        <Footer></Footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
