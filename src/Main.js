ddc805c431b702d9a8b419c1335b432e7bff857e
Cacton12 <Colbyacton12@icloud.com>
2024-11-04 08:33:59 -0400

    added a change

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ const Main = () => {
     return (
       <div className="flex flex-wrap justify-center items-center w-full h-full px-4 sm:px-8 py-4">
       <div className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 text-center p-4">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl raleway-regular text-emerald-600">About Me</h1>
+          <h1 className="text-3xl md:text-4xl lg:text-5xl raleway-regular text-emerald-600">About Me!</h1>
           <p className="mt-2 text-sm sm:text-base md:text-lg lg:text-xl text-gray-200 leading-relaxed">
               Hello! I'm Colby Acton, I grew up in a small town called Sackville, New Brunswick. I moved to pursue a degree in civil engineering at the University of New Brunswick, but I quickly realized that i did not enjoy this field and dropped out.
               <br /><br />
